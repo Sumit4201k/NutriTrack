c1ea680d26b082251b163e4fbfe57fa59ad6a4c0
sumit4201k <kamtisumit6856@gmail.com>
2026-06-21 20:15:52 +0530

    feat: refactor imports and enhance AddMealScreen with haptic feedback; update HomeScreen layout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { colors } from '@/styles/global';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
